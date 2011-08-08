c9c97d458fdd4827ff856f675a16c843861d5085
Ryan Angilly <ryan@angilly.com>
2011-08-07 20:54:25 -0400

    making some changes per http://groups.google.com/group/delayed_job/browse_thread/thread/fd2c9d548f8e6392?pli=1

--- lib/delayed/backend/mongo_mapper.rb
+++ lib/delayed/backend/mongo_mapper.rb
@@ -42,13 +42,20 @@ module Delayed
             :sort => [[:priority, 1], [:run_at, 1]]
           }
 
-          where = "this.locked_at == null || this.locked_at < #{make_date(right_now - max_run_time)}"
+          #where = "this.locked_at == null || this.locked_at < #{make_date(right_now - max_run_time)}"
+          ors = [
+            {:locked_at => nil},
+            {:locked_at => {'$lt' => (right_now - max_run_time)}}
+          ]
 
           (conditions[:priority] ||= {})['$gte'] = Worker.min_priority.to_i if Worker.min_priority
           (conditions[:priority] ||= {})['$lte'] = Worker.max_priority.to_i if Worker.max_priority
 
           results = all(conditions.merge(:locked_by => worker_name))
-          results += all(conditions.merge('$where' => where)) if results.size < limit
+          
+          #results += all(conditions.merge('$where' => where)) if results.size < limit
+          results += all(conditions.merge('$or' => ors)) if results.size < limit
+
           results
         end
 
@@ -63,8 +70,15 @@ module Delayed
           right_now = self.class.db_time_now
           overtime = right_now - max_run_time.to_i
 
-          query = "this.locked_at == null || this.locked_at < #{make_date(overtime)} || this.locked_by == #{worker.to_json}"
-          conditions = {:_id => id, :run_at => {"$lte" => right_now}, "$where" => query}
+          #query = "this.locked_at == null || this.locked_at < #{make_date(overtime)} || this.locked_by == #{worker.to_json}"
+          ors = [
+            {:locked_at => nil},
+            {:locked_at => {'$lt' => overtime}},
+            {:locked_by => worker}
+          ]
+          
+          #conditions = {:_id => id, :run_at => {"$lte" => right_now}, "$where" => query}
+          conditions = {:_id => id, :run_at => {"$lte" => right_now}, "$or" => ors}
 
           collection.update(conditions, {"$set" => {:locked_at => right_now, :locked_by => worker}})
           affected_rows = self.collection.find({:_id => id, :locked_by => worker}).count
